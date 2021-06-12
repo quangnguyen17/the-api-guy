5a01639a9c09d9eab925d501b96691e49a4261f4
Duong Nguyen <duongnguyen@Duongs-MacBook-Pro.local>
2021-06-12 11:29:55 -0700

    updated project

--- src/App.js
+++ src/App.js
@@ -1,123 +1,97 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { NavBar, APIListItem } from "./Components";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { APIListItem } from './Components';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import './App.css';
 
 const App = () => {
-  const [categories, setCategories] = useState([]);
-  const [APIs, setAPIs] = useState([]);
-  const [filteredAPIs, setFilteredAPIs] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [APIs, setAPIs] = useState([]);
+    const [filteredAPIs, setFilteredAPIs] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
 
-  useEffect(() => {
-    axios
-      .get("https://api.publicapis.org/categories")
-      .then(res => setCategories(res.data))
-      .catch(err => console.log(err));
-    axios
-      .get("https://api.publicapis.org/entries")
-      .then(res => setAPIs(res.data.entries))
-      .catch(err => console.log(err));
-  }, []);
+    useEffect(() => {
+        (async () => {
+            try {
+                setCategories((await axios.get('https://api.publicapis.org/categories')).data);
+                setAPIs((await axios.get('https://api.publicapis.org/entries')).data.entries);
+            } catch (err) {
+                alert(JSON.stringify(err));
+            }
+        })();
+    }, []);
 
-  useEffect(() => {
-    if (selectedCategory.length > 0) {
-      setFilteredAPIs(
-        APIs.filter(
-          API => API.Category.toLowerCase() === selectedCategory.toLowerCase()
-        )
-      );
-    }
-  }, [APIs, selectedCategory]);
+    useEffect(() => {
+        if (selectedCategory.length > 0) {
+            setFilteredAPIs(APIs.filter((API) => API.Category.toLowerCase() === selectedCategory.toLowerCase()));
+        }
+    }, [APIs, selectedCategory]);
 
-  useEffect(() => {
-    if (searchText.length > 0) {
-      let searchedRes = [];
-      if (selectedCategory.length > 0) {
-        searchedRes = APIs.filter(API => {
-          const containsSearchText = API.API.toLowerCase().includes(
-            searchText.toLowerCase()
-          );
-          const containsCategory =
-            API.Category.toLowerCase() === selectedCategory.toLowerCase();
-          return containsSearchText && containsCategory;
-        });
-      } else {
-        searchedRes = APIs.filter(API =>
-          API.API.toLowerCase().includes(searchText.toLowerCase())
-        );
-      }
+    useEffect(() => {
+        if (searchText.length > 0) {
+            let searchedRes = [];
+            if (selectedCategory.length > 0) {
+                searchedRes = APIs.filter((API) => {
+                    const containsSearchText = API.API.toLowerCase().includes(searchText.toLowerCase());
+                    const containsCategory = API.Category.toLowerCase() === selectedCategory.toLowerCase();
+                    return containsSearchText && containsCategory;
+                });
+            } else {
+                searchedRes = APIs.filter((API) => API.API.toLowerCase().includes(searchText.toLowerCase()));
+            }
 
-      setFilteredAPIs(searchedRes);
-    } else {
-      if (selectedCategory.length > 0) {
-        const filtered = APIs.filter(
-          API => API.Category.toLowerCase() === selectedCategory.toLowerCase()
-        );
-        setFilteredAPIs(filtered);
-      }
-    }
-  }, [searchText, APIs, selectedCategory]);
+            setFilteredAPIs(searchedRes);
+        } else {
+            if (selectedCategory.length > 0) {
+                const filtered = APIs.filter((API) => API.Category.toLowerCase() === selectedCategory.toLowerCase());
+                setFilteredAPIs(filtered);
+            }
+        }
+    }, [searchText, APIs, selectedCategory]);
 
-  return (
-    <div className="App">
-      <Container fluid className="p-5">
-        <NavBar />
-        <Row className="mt-5">
-          <Col md={9} className="pr-5">
-            <input
-              type="text"
-              onChange={event => setSearchText(event.target.value)}
-              placeholder="º Search for APIs"
-              className="w-100 h3 mb-5 border-0 font-weight-normal"
-            />
-            {selectedCategory.length > 0 && (
-              <Button
-                onClick={() => setSelectedCategory("")}
-                size="sm"
-                variant="info"
-                className="py-1 px-2 align-middle mr-3 mb-4"
-              >
-                Show All
-              </Button>
-            )}
-            <h6 className="d-inline-block align-middle mb-4">
-              Showing{" "}
-              {selectedCategory.length > 0 || searchText.length > 0
-                ? filteredAPIs.length
-                : APIs.length}{" "}
-              APIs{" "}
-              {selectedCategory.length > 0 ? `for ${selectedCategory}` : ""}
-            </h6>
-            <ul className="list-inline">
-              {selectedCategory.length > 0 || searchText.length > 0
-                ? filteredAPIs.map((API, idx) => (
-                    <APIListItem key={idx} API={API} />
-                  ))
-                : APIs.map((API, idx) => <APIListItem key={idx} API={API} />)}
-            </ul>
-          </Col>
-          <Col md={3}>
-            <h5>{categories.length} Categories</h5>
-            <ul className="list-inline mt-4">
-              {categories.map((category, idx) => (
-                <Button
-                  onClick={event => setSelectedCategory(event.target.innerText)}
-                  key={idx}
-                  className="list-inline-item m-1 py-1 px-2 rounded"
-                  variant={selectedCategory === category ? "dark" : "light"}
-                >
-                  {category}
-                </Button>
-              ))}
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container fluid className="p-4">
+                <Row>
+                    <Col md={3} className="pr-5">
+                        <h5 className="font-weight-bold text-secondary mb-2">{categories.length} Categories</h5>
+                        <ul className="list-inline">
+                            {categories.map((category, idx) => (
+                                <Button
+                                    key={idx}
+                                    onClick={(event) => setSelectedCategory(event.target.innerText)}
+                                    className="list-inline-item m-1 py-1 px-2 rounded"
+                                    variant={selectedCategory === category ? 'dark' : 'light'}
+                                >
+                                    {category}
+                                </Button>
+                            ))}
+                        </ul>
+                    </Col>
+                    <Col md={9}>
+                        <h1>The API Guy.</h1>
+                        <input type="text" onChange={(event) => setSearchText(event.target.value)} placeholder="º Search for APIs" className="searchBar" />
+                        {selectedCategory.length > 0 && (
+                            <Button onClick={() => setSelectedCategory('')} size="sm" variant="info" className="py-1 px-2 align-middle mr-3 mb-4">
+                                Show All
+                            </Button>
+                        )}
+                        <h6 className="d-inline-block align-middle text-secondary mb-3">
+                            {searchText.length > 0 ? 'Found' : 'Showing'}{' '}
+                            {selectedCategory.length > 0 || searchText.length > 0 ? filteredAPIs.length : APIs.length} APIs{' '}
+                            {selectedCategory.length > 0 ? `for ${selectedCategory}` : ''}
+                        </h6>
+                        <ul className="list-inline">
+                            {selectedCategory.length > 0 || searchText.length > 0
+                                ? filteredAPIs.map((API, idx) => <APIListItem key={idx} API={API} />)
+                                : APIs.map((API, idx) => <APIListItem key={idx} API={API} />)}
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default App;
--- src/Components.js
+++ src/Components.js
@@ -1,73 +1,37 @@
-import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
-export const NavBar = () => (
-  <Navbar>
-    <h1 className="display-4">The API Guy.</h1>
-    <Nav className="ml-auto justify-content-end">
-      <Nav.Link>What is an API?</Nav.Link>
-      <Nav.Link>How to use it?</Nav.Link>
-    </Nav>
-  </Navbar>
-);
+const lower = (str) => str.replace(/^\w/, (c) => c.toLowerCase());
 
 export const APIListItem = ({ API }) => {
-  const lower = str => str.replace(/^\w/, c => c.toLowerCase());
-
-  const TrueFalse = text => (
-    <pre
-      className={`d-inline-block align-middle text-${
-        text === "true" ? "success" : "danger"
-      }`}
-    >
-      &#60;{text === "true" ? "supported" : "not-supported"}&#62;
-    </pre>
-  );
-  const Cors = text => (
-    <pre
-      className={`d-inline-block align-middle text-${
-        text === "no" ? "danger" : text === "unknown" ? "warning" : "success"
-      }`}
-    >
-      &#60;{lower(text)}&#62;
-    </pre>
-  );
-
-  return (
-    <li
-      style={{ minWidth: 275, width: "auto", maxWidth: 375 }}
-      className="list-inline-item align-top mr-5 mb-5"
-    >
-      <h1>{API.API}</h1>
-      <p className="text-secondary">
-        #{API.Category.replace(/ /g, "").toLowerCase()}
-      </p>
-      <ul style={{ listStylePosition: "inside" }} className="mt-3">
-        <li>
-          <span className="align-middle mr-2">Auth:</span>
-          <pre className="d-inline-block align-middle text-info">
-            &#60;{API.Auth.length < 1 ? "none" : lower(API.Auth)}&#62;
-          </pre>
-        </li>
-        <li>
-          <span className="align-middle mr-2">HTTPS:</span>
-          {TrueFalse(JSON.stringify(API.HTTPS))}
-        </li>
-        <li>
-          <span className="align-middle mr-2">Cors:</span>
-          {Cors(API.Cors)}
-        </li>
-      </ul>
-      <p className="my-2">{API.Description}</p>
-      <Button
-        variant="link"
-        href={API.Link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-secondary"
-      >
-        Read Docs
-      </Button>
-    </li>
-  );
+    return (
+        <div className="api-card">
+            <div className="w-100 d-flex justify-content-between align-items-center">
+                <div className="mr-auto">
+                    <h1>{API.API}</h1>
+                    <p className="text-secondary">#{API.Category.replace(/ /g, '').toLowerCase()}</p>
+                    <p className="my-2">{API.Description}</p>
+                </div>
+                <Button variant="light" href={API.Link} className="px-2 py-1 mb-auto">
+                    Docs
+                </Button>
+            </div>
+            <div className="mt-2">
+                <div className="mb-1">
+                    <span className="info-key">Auth:</span>
+                    <code className="info-val text-info">{API.Auth.length < 1 ? 'none' : lower(API.Auth)}</code>
+                </div>
+                <div className="mb-1">
+                    <span className="info-key">HTTPS:</span>
+                    <code className={`info-val ${API.HTTPS === 'true' ? 'text-success' : 'text-danger'}`}>
+                        {API.HTTPS === 'true' ? 'supported' : 'not-supported'}
+                    </code>
+                </div>
+                <div className="mb-1">
+                    <span className="info-key">Cors:</span>
+                    <code className={`info-val ${API.Cors === 'yes' ? 'text-success' : 'text-danger'}`}>{API.Cors}</code>
+                </div>
+            </div>
+        </div>
+    );
 };
